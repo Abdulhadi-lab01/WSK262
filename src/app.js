17f5375bc6c9f57a8465f970ddf213abf04b88bd
Abdulhadi Younisalkurdi <iqabduhd@gmail.com>
2026-03-31 11:52:33 +0200

    done

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import api from './api/index.js';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use('/api/v1', api);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my REST API!');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    res.json({ok: true, data: req.body});
+});
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
+export default app;
